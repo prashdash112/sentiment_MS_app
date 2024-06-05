6b35f90db3c4d63792f8bcde302c3df551b1eba2
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2024-06-05 19:40:47 +0530

    updated app.py

--- app.py
+++ app.py
@@ -18,29 +18,34 @@ Define a GitHub workflow to run linting checks and tests for the built RESTful A
 '''
 
 # Author: Prashant Singh
-# Date: 4th June, 2024
+# Date: 5th June, 2024
 
 import requests
 from textblob import TextBlob 
+from flask import Flask, request, jsonify
+import datetime
+
+
+app = Flask(__name__)
 
 
 def get_api_response_requests(url):
+
     '''
     Function to accept a url and returns a response in JSON format
     
-    url: String
+    Input: url String
     returns: JSON 
+
     '''
 
     try:
-        # make the GET request
         response = requests.get(url)
-        
         # Raise an HTTPError for bad responses
         response.raise_for_status()
-        
         # Parse the response as JSON
         response_json = response.json()
+        
         return response_json
     
     except requests.exceptions.HTTPError as http_err:
@@ -55,10 +60,11 @@ def analyze_sentiment(text):
     '''
     
     Function to analyze the sentiment of the text using textblob library
-    text: String 
+    Input: String 
     returns: float, string
 
     '''
+
     # Create a textblob object
     blob = TextBlob(text)
     
@@ -75,53 +81,93 @@ def analyze_sentiment(text):
     
     return polarity, sentiment
 
-# URLs
-url_subfeddit_names = "http://localhost:8080/api/v1/subfeddits/?skip=0&limit=10"
-url_subfeddit_comments = "http://localhost:8080/api/v1/comments/?subfeddit_id=1&skip=0&limit=10"
+# Function to convert Unix timestamp to 'YYYYMMDD' format
+def convert_timestamp_to_text(timestamp):
+    
+    '''
+    
+    Function converts a unix timestamp into a datetime object with format: YYYY-MM-DD
+    Input: Timestamp object
+    Output: Timestamp object
 
+    '''
+    
+    date_time = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
+    formatted_date = date_time.strftime('%Y%m%d')
+    return formatted_date
 
+#URL
+url_subfeddit_names = "http://localhost:8080/api/v1/subfeddits/?skip=0"
 response_subfeddit_names = get_api_response_requests(url_subfeddit_names)
-response_subfeddit_comments = get_api_response_requests(url_subfeddit_comments)
 
 # Access the list of subfeddits
 subfeddits = response_subfeddit_names['subfeddits']
-# Extract usernames and ids using list comprehension
+# Mapping subfeddit names to ids:
 usernames = [subfeddit['username'] for subfeddit in subfeddits]
-ids = [subfeddit['id'] for subfeddit in subfeddits] 
-
-# Sort the 'comments' list to extract the recent comments 
-response_subfeddit_comments['comments'].sort(key=lambda comment: comment['created_at'], reverse=True)
-#print(response_subfeddit_comments)
-comments  = response_subfeddit_comments['comments'] 
-recent_comment_text = [comment_text['text'] for comment_text in comments]
-recent_comment_id = [comment_id['id'] for comment_id in comments]
-
-polarity_scores = []
-sentiment_vals = []
-for comment in recent_comment_text:
-    polarity, sentiment = analyze_sentiment(comment)
-    polarity_scores.append(polarity)
-    sentiment_vals.append(sentiment)
+ids = {subfeddit["username"]:subfeddit['id'] for subfeddit in subfeddits }
 
 
-final_results = []
-for comment, comment_id, polarity_score, sentiment_val in zip(recent_comment_text,recent_comment_id, polarity_scores, sentiment_vals):
-    vals_dict = {"Unique_identifier_of_comment": comment_id,
-                 "text_comment": comment,
-                 "polarity_score":polarity_score,
-                 "sentiment_class": sentiment_val
-                }
-    final_results.append(vals_dict)
+@app.route("/polarity/<subfeddit>",methods=(['GET']))
+def polarity_scores(subfeddit):
 
+    # URL
+    url_subfeddit_comments = f"http://localhost:8080/api/v1/comments/?subfeddit_id={ids[subfeddit]}&skip=0&limit=22000"
+    response_subfeddit_comments = get_api_response_requests(url_subfeddit_comments)
 
-# Print the extracted usernames
-print(usernames)
-print(ids)
-print('\n')
+    # Creating querry parameters for the comments
+    sort = request.args.get("sort")
+    limit = request.args.get("limit")
+    date_range = request.args.get('date_range', '')
 
-print(recent_comment_text)           
-print("Final Results:\n\n", final_results) 
-
+    # Sort the 'comments' list to extract the recent comments 
+    response_subfeddit_comments['comments'].sort(key=lambda comment: comment['created_at'], reverse=True)
     
+    comments  = response_subfeddit_comments['comments'] 
+    recent_comment_text = [comment_text['text'] for comment_text in comments]
+    recent_comment_id = [comment_id['id'] for comment_id in comments]
+    created_at_comment = [comment_created['created_at'] for comment_created in comments]
+    formatted_date = [convert_timestamp_to_text(timestamp) for timestamp in created_at_comment]
+    
+    print("Formatted date:",formatted_date)
 
+    polarity_scores = []
+    sentiment_vals = []
+    for comment in recent_comment_text:
+        polarity, sentiment = analyze_sentiment(comment)
+        polarity_scores.append(polarity)
+        sentiment_vals.append(sentiment)
+
+    final_results = []
+    for comment, comment_id, polarity_score, sentiment_val, date in zip(recent_comment_text,recent_comment_id, polarity_scores, sentiment_vals,formatted_date):
+        vals_dict = {"UID_comment": comment_id,
+                    "text_comment": comment,
+                    "polarity_score":polarity_score,
+                    "sentiment_class": sentiment_val,
+                    "date": date
+                    }
+        final_results.append(vals_dict)
+
+    # Slicing the comments data using the date range
+    if date_range:
+        try:
+            start_date_str, end_date_str = date_range.split(',')
+            print("Dates string:",start_date_str,end_date_str)
+            final_results = [item for item in final_results if start_date_str <= item['date'] <= end_date_str]
+        except ValueError:
+            return jsonify({'error': 'Invalid date_range format. Use DDMMYYYY,DDMMYYYY format to enter the date range'}), 400
+        
+    if limit: 
+        final_results = final_results[:int(limit)]
+    if sort:
+        final_results.sort(key = lambda item: item["polarity_score"], reverse = sort=="desc")
+    
+    # Print the extracted ids and final results on terminal 
+    print(ids)
+    print('\n')
+    print("Final Results:\n\n", final_results) 
+    
+    return jsonify(final_results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
