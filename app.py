d3ee05b260382f4be44899ec4497d616ab7bf9d8
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2024-06-05 20:21:15 +0530

    linting checks

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 '''
 
 Problem Statement
-You are asked to design and develop a small microservice application offering a RESTful API that identifies if comments on a given subfeddit or category are positive or negative.
+You are asked to design and develop a small microservice application offering a 
+RESTful API that identifies if comments on a given subfeddit or category are positive or negative.
 
 Given the name of a subfeddit the application should return:
 A list of the most recent comments. Suppose a limit of 25 comments.
 For each comment:
 The unique identifier of the comment
 The text of the comment
-The polarity score and the classification of the comment (positive, or negative) based on that score.
+
+The polarity score and the classification of the comment (positive, or negative) based on 
+that score.
 
 Optionally, you should allow the user to modify the query as follows:
 Filter comments by a specific time range 
@@ -21,7 +24,7 @@ Define a GitHub workflow to run linting checks and tests for the built RESTful A
 # Date: 5th June, 2024
 
 import requests
-from textblob import TextBlob 
+from textblob import TextBlob
 from flask import Flask, request, jsonify
 import datetime
 
@@ -35,7 +38,7 @@ def get_api_response_requests(url):
     Function to accept a url and returns a response in JSON format
     
     Input: url String
-    returns: JSON 
+    returns: JSON
 
     '''
 
@@ -45,9 +48,9 @@ def get_api_response_requests(url):
         response.raise_for_status()
         # Parse the response as JSON
         response_json = response.json()
-        
+
         return response_json
-    
+
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
 
@@ -58,7 +61,7 @@ def get_api_response_requests(url):
 def analyze_sentiment(text):
 
     '''
-    
+
     Function to analyze the sentiment of the text using textblob library
     Input: String 
     returns: float, string
@@ -67,10 +70,10 @@ def analyze_sentiment(text):
 
     # Create a textblob object
     blob = TextBlob(text)
-    
+
     # Get the polarity score
     polarity = blob.sentiment.polarity
-    
+
     # Classify the sentiment as positive or negative
     if polarity > 0:
         sentiment = "Positive"
@@ -78,12 +81,12 @@ def analyze_sentiment(text):
         sentiment = "Negative"
     else:
         sentiment = "Neutral"
-    
+
     return polarity, sentiment
 
 # Function to convert Unix timestamp to 'YYYYMMDD' format
 def convert_timestamp_to_text(timestamp):
-    
+
     '''
     
     Function converts a unix timestamp into a datetime object with format: YYYY-MM-DD
@@ -91,7 +94,7 @@ def convert_timestamp_to_text(timestamp):
     Output: Timestamp object
 
     '''
-    
+
     date_time = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
     formatted_date = date_time.strftime('%Y%m%d')
     return formatted_date
@@ -121,13 +124,13 @@ def polarity_scores(subfeddit):
 
     # Sort the 'comments' list to extract the recent comments 
     response_subfeddit_comments['comments'].sort(key=lambda comment: comment['created_at'], reverse=True)
-    
-    comments  = response_subfeddit_comments['comments'] 
+
+    comments  = response_subfeddit_comments['comments']
     recent_comment_text = [comment_text['text'] for comment_text in comments]
     recent_comment_id = [comment_id['id'] for comment_id in comments]
     created_at_comment = [comment_created['created_at'] for comment_created in comments]
     formatted_date = [convert_timestamp_to_text(timestamp) for timestamp in created_at_comment]
-    
+
     print("Formatted date:",formatted_date)
 
     polarity_scores = []
@@ -138,7 +141,7 @@ def polarity_scores(subfeddit):
         sentiment_vals.append(sentiment)
 
     final_results = []
-    for comment, comment_id, polarity_score, sentiment_val, date in zip(recent_comment_text,recent_comment_id, polarity_scores, sentiment_vals,formatted_date):
+    for comment, comment_id, polarity_score, sentiment_val, date in zip(recent_comment_text,recent_comment_id,polarity_scores, sentiment_vals,formatted_date):
         vals_dict = {"UID_comment": comment_id,
                     "text_comment": comment,
                     "polarity_score":polarity_score,
@@ -155,17 +158,17 @@ def polarity_scores(subfeddit):
             final_results = [item for item in final_results if start_date_str <= item['date'] <= end_date_str]
         except ValueError:
             return jsonify({'error': 'Invalid date_range format. Use YYYYMMDD,YYYYMMDD format to enter the date range'}), 400
-        
-    if limit: 
+
+    if limit:
         final_results = final_results[:int(limit)]
     if sort:
         final_results.sort(key = lambda item: item["polarity_score"], reverse = sort=="desc")
-    
+
     # Print the extracted ids and final results on terminal 
     print(ids)
     print('\n')
-    print("Final Results:\n\n", final_results) 
-    
+    print("Final Results:\n\n", final_results)
+
     return jsonify(final_results)
 
 if __name__ == "__main__":
